1c0c0860ce4799addc5572f1fc470c50cdd09bef
ry6n1k <levchenkoyarya@gmail.com>
2023-11-13 13:38:11 +0200

    connect DB

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,5 +1,9 @@
 package com.example.library;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
+		
+
+		String url = "jdbc:mysql://localhost:3306/library?autoReconnect=true&useSSL=false";
+        String username = "admin";
+        String password = "password";
+        System.out.println("Connecting...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+
 		SpringApplication.run(LibraryApplication.class, args);
 	}
 
